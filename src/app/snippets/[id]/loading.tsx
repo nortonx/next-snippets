a2fcd0ae85fb02be3dfba35203fcace9354ed309
Norton Almeida <norton.almeida@gmail.com>
2025-03-27 21:56:59 -0300

    feat: new thing

---
+++ src/app/snippets/[id]/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Loading...</h1>
+      <p className="mt-4 text-gray-500">Please wait while we fetch the data.</p>
+    </div>
+  );
+}
