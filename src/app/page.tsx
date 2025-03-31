057c5bea9b2d9507064b1ab3a9ea31411ba9873b
Norton Almeida <norton.almeida@gmail.com>
2025-03-31 10:34:57 -0300

    feat(implementing-on-demand-cache): Implemented cache on demand for next.js app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,31 @@
-import Link from "next/link";
-import { db } from "@/db";
+import Link from 'next/link';
+import { db } from '@/db';
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
-  const renderedSnippets = snippets.map((snippet) => (
-    <Link 
-      key={snippet.id} 
-      className="font-mono bg-gray-100 rounded underline m-2"
-      href={`/snippets/${snippet.id}`}
-    >
-      <h2>{snippet.title}({snippet.id})</h2>
-    </Link>
-  ));
+  const renderedSnippets = snippets.map((snippet) => {
+    return (
+      <Link
+        key={snippet.id}
+        href={`/snippets/${snippet.id}`}
+        className="flex justify-between items-center p-2 border rounded"
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
+  });
 
   return (
     <div>
-      {renderedSnippets}
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href="/snippets/new" className="border p-2 rounded">
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
     </div>
   );
 }
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,53 +1,57 @@
-import { notFound, redirect } from "next/navigation";
-import { db } from "@/db";
-export default function SnippetCreatePage() {
+"use client";
+import { useActionState, startTransition } from "react";
 
-  async function createSnippet(formData: FormData) {
-    // This needs to be server action
-    'use server';
-    // check the user inputs and make sure they're valid
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-    // create a new snippet in the database
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
+import * as actions from "@/actions";
+
+export default function SnippetCreatePage() {
+  const [formState, action] = useActionState(actions.createSnippet, {
+    message: "",
+  });
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    startTransition(() => {
+      action(formData);
     });
-    console.log("Snippet created", snippet);
-    // redirect user to home page
-    redirect("/");
   }
 
-  notFound();
-
   return (
-    <div>
-      <form action={createSnippet}>
-        <h3 className="font-bold m-3">Create a snippet</h3>
-        <div className="flex flex-col gap-4">
-          <div className="flex gap-4">
-            <label htmlFor="title" className="w-12">Title</label>
-            <input 
-              type="text"
-              id="title"
-              name="title"
-              className="border rounded p-2 w-full"
-            />
-          </div>
-
-          <div className="flex gap-4">
-            <label htmlFor="code" className="w-12">Code</label>
-            <textarea
-              id="code"
-              name="code"
-              className="border rounded p-2 w-full"
-            />
-          </div>
-          <button type="submit" className="rounded p-2 bg-blue-200">Create</button>
+    <form onSubmit={handleSubmit}>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            name="title"
+            className="border rounded p-2 w-full"
+            id="title"
+          />
         </div>
-      </form>
-    </div>
+
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            name="code"
+            className="border rounded p-2 w-full"
+            id="code"
+          />
+        </div>
+
+        {formState.message ? (
+          <div className="my-2 p-2 bg-red-200 border rounded border-red-400">
+            {formState.message}
+          </div>
+        ) : null}
+
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
   );
 }
